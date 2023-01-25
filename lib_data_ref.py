63e1c610fe38b92eb727931a05c621f5e3369808
Nicolas CARAYON <nicolas.carayon@gmail.com>
2023-01-25 18:37:24 +0100

    Complete analysis of gravité/usagers and gravité/caracteristiques

--- lib_data_ref.py
+++ lib_data_ref.py
@@ -100,3 +100,101 @@ def get_labels(varname, value):
             return 'Autre collision'
         if value == 7:
             return 'Sans collision'
+
+    if varname == 'trajet':
+        if value == -1: 
+            return 'Non renseigné'
+        if value == 0:
+            return 'Non renseigné'
+        if value == 1:
+            return 'Domicile - travail'
+        if value == 2:
+            return  'Domicile - école'
+        if value == 3:
+            return 'Courses - achats'
+        if value == 4:
+            return  'Utilisation professionnelle'
+        if value == 5:
+            return 'Promende - loisirs'
+        if value == 9:
+            return 'Autre'
+    
+    if (varname == 'secu1') or (varname == 'secu2') or (varname == 'secu3'):
+        if value == -1:
+            return 'Non renseigné'
+        if value == 0:
+            return 'Aucun équipement'
+        if value == 1:
+            return 'Ceinture'
+        if value == 2:
+            return 'Casque'
+        if value == 3:
+            return 'Dispositif enfants'
+        if value == 4:
+            return 'Gilet réfléchissant'
+        if value == 5:
+            return 'Airbag (2RM/3RM)'
+        if value == 6:
+            return 'Gants (2RM/3RM)'
+        if value == 7:
+            return 'Non déterminable'
+        if value == 8:
+            return 'Autre'
+
+    if varname == 'locp':
+        if value == -1:
+            return 'Non renseigné'
+        if value == 0:
+            return 'Sans objet'
+        if value == 1:
+            return 'Sur chaussée - A +50 du pass piéton'
+        if value == 2:
+            return 'Sur chaussée - A -50 du pass piéton'
+        if value == 3:
+            return 'Sur pass piéton - Sans signalisation lumineuse'
+        if value == 4:
+            return 'Sur pass piéton - Avec signalisation lumineuse'
+        if value == 5:
+            return 'Sur trottoir'
+        if value == 6:
+            return 'Sur accotement'
+        if value == 7:
+            return 'Sur refuge ou BAU'
+        if value == 8:
+            return 'Sur contre allée'
+        if value == 9:
+            return 'Inconnue'
+
+    if varname == 'actp':
+        if value == '-1':
+            return 'Non renseigné'
+        if value == '0':
+            return 'Se déplaçant - Non renseigné ou sans objet'
+        if value == '1':
+            return 'Se déplaçant - Sens véhicule ou heurtant'
+        if value == '2':
+            return 'Se déplaçant - Sens inverse du véhicule'
+        if value == '3':
+            return 'Traversant'
+        if value == '4':
+            return 'Masqué'
+        if value == '5':
+            return 'Jouant - courant'
+        if value == '6':
+            return 'Avec animal'
+        if value == '9':
+            return 'Autre'
+        if value == 'A':
+            return 'Monte/descend du véhicule'
+        if value == 'B':
+            return 'Inconnue'   
+
+    if varname == 'etatp':
+        if value == -1:
+            return 'Non renseigné'
+        if value == 1:
+            return 'Seul'
+        if value == 2:
+            return 'Accompagné'
+        if value == 3:
+            return 'En groupe'
