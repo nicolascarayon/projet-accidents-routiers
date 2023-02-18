78678576f903c2933e0d75b1aeef51593bf45d9e
Nicolas Carayon <nicolas.carayon@gmail.com>
2023-02-18 16:26:19 +0100

    Replace SMOTE with RandomOverSampler

--- my_libs/encoder_custom.py
+++ my_libs/encoder_custom.py
@@ -1,6 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from category_encoders import TargetEncoder, OneHotEncoder
-from imblearn.over_sampling import SMOTE
+from imblearn.over_sampling import SMOTEN, SMOTE, SMOTENC, RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
 from sklearn.preprocessing import StandardScaler
 
 import pandas as pd
@@ -9,28 +10,31 @@ class EncoderCustom(BaseEstimator, TransformerMixin):
         self.trace = trace
         self.cols_target_encoded = cols_target_encoded
         self.target_encoder = TargetEncoder()
-
         self.encoder_target = TargetEncoder(cols=cols_target_encoded)
         self.encoder_onehot = OneHotEncoder(cols=cols_onehot_encoded)
         self.scaler = StandardScaler()
-        self.sampler = SMOTE()
-        # sampler        = RandomUnderSampler()
-        # sampler        = RandomOverSampler()
+        # self.sampler = SMOTENC()
+        self.sampler        = RandomUnderSampler()
+        # self.sampler        = RandomOverSampler()
 
     def fit(self, X, y):
         return self
 
     def transform(self, X, y, datatype='Train'):
-        if datatype == 'Train':
-            X_te = self.encoder_target.fit_transform(X, y)
-            X_oh = self.encoder_onehot.fit_transform(X_te, y)
-            X_sc = self.scaler.fit_transform(X_oh)
-            X_rs, y_rs = self.sampler.fit_resample(X_sc, y)
-        if datatype == 'Test':
-            X_te  = self.encoder_target.transform(X)
-            X_oh  = self.encoder_onehot.transform(X_te)
-            X_sc  = self.scaler.transform(X_oh)
-            X_rs, y_rs = self.sampler.fit_resample(X_sc, y)
 
-        # return pd.DataFrame(data=X_sc, columns=X_oh.columns), y
-        return pd.DataFrame(data=X_rs, columns=X_oh.columns), y_rs
+        # self.sampler = SMOTEN()
+
+        if datatype == 'Train':
+            X = self.encoder_target.fit_transform(X, y)
+            X = self.encoder_onehot.fit_transform(X, y)
+            X = self.scaler.fit_transform(X)
+            X, y = self.sampler.fit_resample(X, y)
+
+        if datatype == 'Test':
+            X  = self.encoder_target.transform(X)
+            X  = self.encoder_onehot.transform(X)
+            X = self.scaler.transform(X)
+
+        return pd.DataFrame(data=X, columns=self.encoder_onehot.get_feature_names()), y
+
+#%%
