41ec3b3b6fb1d57f333deee75d63751e8681fd10
nicolascarayon <nicolas.carayon@gmail.com>
2023-03-02 16:38:05 +0100

    Create labels references for Shapash

--- my_libs/ref_labels.py
+++ my_libs/ref_labels.py
@@ -1,32 +1,100 @@
 import pandas as pd
 
-dic_features = {'grav'   : 'Gravité',
-                'place ' : "Place de l'usager",
-                'catu'   : "Catégorie de l'usager",
-                'sexe'   : "sexe de l'usager",
-                'trajet' : 'Motif du déplacement',
-                'locp'   : 'Localisation du piéton',
-                'actp'   : 'Action du piéton',
-                'etatp'  : 'Piéton seul?',
-                'mois'   : 'Mois',
-                'lum'    : 'Conditions de luminosité',
-                'agg'    : 'Localisation',
-                'int'    : 'Intersection',
-                'atm'    : 'Conditions atmosphériques',
-                'col'    : 'Type de collision',
-                'dep'    : 'Département',
-                'catr'   : 'Catégorie de route',
-                'circ'   : 'Conditions de circulation',
-                'nbv'    : 'Nombre de voies',
-                'vosp'   : 'Voie réservée?',
-                'prof'   : 'profil de la route',
-                'plan'   : 'tracé en plan',
-                'surf'   : 'Etat de la surface',
-                'infra'  : 'Aménagement - Infrastructure',
-                'situ'   : "Situation de l'accident",
-                'catv'   : 'Catégorie de véhicule',
+# Labels for Shapash smart explainer
+
+dic_target = {0: 'Indemne - Blessé léger',
+              1: 'Tué - Blessé hospitalisé'}
+
+dic_features = {'grav': 'Gravité',
+                'place ': "Place de l'usager",
+                'catu': "Catégorie de l'usager",
+                'sexe': "sexe de l'usager",
+                'trajet': 'Motif du déplacement',
+                'locp': 'Localisation du piéton',
+                'actp': 'Action du piéton',
+                'etatp': 'Piéton seul?',
+                'mois': 'Mois',
+                'lum': 'Conditions de luminosité',
+                'agg': 'Localisation',
+                'int': 'Intersection',
+                'atm': 'Conditions atmosphériques',
+                'col': 'Type de collision',
+                'dep': 'Département',
+                'catr': 'Catégorie de route',
+                'circ': 'Conditions de circulation',
+                'nbv': 'Nombre de voies',
+                'vosp': 'Voie réservée?',
+                'prof': 'profil de la route',
+                'plan': 'tracé en plan',
+                'surf': 'Etat de la surface',
+                'infra': 'Aménagement - Infrastructure',
+                'situ': "Situation de l'accident",
+                'catv': 'Catégorie de véhicule',
                 'age_cls': "Classe d'âge",
                 'joursem': 'Jour de la semaine'}
+
+dic_preproc = [{'col': 'catv',
+                'mapping': pd.Series(data=[-1, 1, 2, 7, 10, 30, 33],
+                                     index=['Autres', 'Bicyclette', 'Cyclomoteur <50cm3', 'VL seul',
+                                            'VU seul', 'Scooter <50cm3', 'Motocyclette >125cm3']),
+                'data_type': 'object'},
+               {'col': 'agg',
+                'mapping': pd.Series(data=[1, 2], index=['Hors agglomération', 'En agglomération']),
+                'data_type': 'object'},
+               {'col': 'col',
+                'mapping': pd.Series(data=[-1, 1, 2, 3, 4, 5, 6, 7],
+                                     index=['Non renseigné', '2 véhic-frontale', "2 véhic-par l'arrière",
+                                            '2 vehic-par le côté', '3 vehic et +- en chaine',
+                                            '3 vehic et plus - collisions multiples',
+                                            'Autre collision', 'Sans collision']),
+                'data_type': 'object'},
+               {'col': 'catr',
+                'mapping': pd.Series(data=[1, 2, 3, 4, 5, 6, 7, 9],
+                                     index=['Autoroute', 'Route nationale', 'Route départementale',
+                                            'Voies communales', 'Hors réseau public', 'Parc de stationnement',
+                                            'Routes de métropole urbaine', 'Autre']),
+                'data_type': 'object'},
+               {'col': 'catu',
+                'mapping': pd.Series(data=[-1, 1, 2, 3], index=['Non renseigné', 'Conducteur', 'Passager', 'Piéton']),
+                'data_type': 'object'},
+               {'col': 'trajet',
+                'mapping': pd.Series(data=[-1, 0, 1, 2, 3, 4, 5, 9],
+                                     index=['Non renseigné', 'Non renseigné', 'Domicile-travail',
+                                            'Domicile-école', 'Courses-achats', 'Utilisation professionnelle',
+                                            'Promenade-Loisirs', 'Autre']),
+                'data_type': 'object'},
+               {'col': 'locp',
+                'mapping': pd.Series(data=[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                                     index=['Non renseigné', 'Sans objet', 'A + 50 m du passage piéton',
+                                            'A – 50 m du passage piéton',
+                                            'Sur passage piéton sans signalisation lumineuse',
+                                            'Sur passage piéton avec signalisation lumineuse',
+                                            'Sur trottoir', 'Sur accotement', 'Sur refuge ou BAU',
+                                            'Sur contre allée', 'Inconnue', ]),
+                'data_type': 'object'},
+               {'col': 'circ',
+                'mapping': pd.Series(data=[-1, 1, 2, 3, 4], index=['Non renseigné', 'A sens unique', 'Bidirectionnelle',
+                                                                   'A chaussées séparées',
+                                                                   'Avec voies d’affectation variable']),
+                'data_type': 'object'},
+
+               {'col': 'situ',
+                'mapping': pd.Series(data=[-1, 0, 1, 2, 3, 4, 5, 6, 8], index=['Non renseigné', 'Aucun', 'Sur chaussée',
+                                                                               'Sur bande d’arrêt d’urgence',
+                                                                               'Sur accotement', 'Sur trottoir',
+                                                                               'Sur piste cyclable',
+                                                                               'Sur autre voie spéciale', 'Autres']),
+                'data_type': 'object'},
+               {'col': 'lum',
+                'mapping': pd.Series(data=[1, 2, 3, 4, 5],
+                                     index=['Plein jour', 'Crépuscule ou aube', 'Nuit sans éclairage public',
+                                            'Nuit avec éclairage public non allumé',
+                                            'Nuit avec éclairage public allumé']),
+                'data_type': 'object'},
+               {'col': 'age_cls',
+                'mapping': pd.Series(data=[0, 1, 2, 3, 4], index=['0-15', '16-25', '26-45', '46-65', '66-']),
+                'data_type': 'object'},
+               ]
 
 dic_catv = {-1: 'Autres',
             1: 'Bicyclette',
@@ -172,28 +240,19 @@ dic_locp = {'def': 'Localisation du piéton',
             9: 'Inconnue'}
 
 
-dic_preproc = [{'col': 'catv',
-                'mapping': pd.Series(data=[-1,1,2,7,10,30,33], index=['Autres','Bicyclette','Cyclomoteur <50cm3','VL seul','VU seul','Scooter <50cm3','Motocyclette >125cm3']),
-                'data_type':'object'},
-               {'col': 'agg',
-                'mapping': pd.Series(data=[1,2], index=['Hors agglomération','En agglomération']),
-                'data_type':'object'},
-               ]
-
-
 def get_labels(varname, value):
-    if varname == 'lum'   : return dic_lum[value]
-    if varname == 'grav'  : return dic_grav[value]
-    if varname == 'catu'  : return dic_catu[value]
-    if varname == 'sexe'  : return dic_sexe[value]
-    if varname == 'agg'   : return dic_agg[value]
-    if varname == 'int'   : return dic_int[value]
-    if varname == 'atm'   : return dic_atm[value]
-    if varname == 'col'   : return dic_col[value]
+    if varname == 'lum': return dic_lum[value]
+    if varname == 'grav': return dic_grav[value]
+    if varname == 'catu': return dic_catu[value]
+    if varname == 'sexe': return dic_sexe[value]
+    if varname == 'agg': return dic_agg[value]
+    if varname == 'int': return dic_int[value]
+    if varname == 'atm': return dic_atm[value]
+    if varname == 'col': return dic_col[value]
     if varname == 'trajet': return dic_trajet[value]
-    if varname == 'actp'  : return dic_actp[value]
-    if varname == 'etatp' : return dic_etatp[value]
-    if varname == 'secu1' : return dic_secu1[value]
-    if varname == 'secu2' : return dic_secu2[value]
-    if varname == 'secu3' : return dic_secu3[value]
-    if varname == 'locp'  : return dic_locp[value]
+    if varname == 'actp': return dic_actp[value]
+    if varname == 'etatp': return dic_etatp[value]
+    if varname == 'secu1': return dic_secu1[value]
+    if varname == 'secu2': return dic_secu2[value]
+    if varname == 'secu3': return dic_secu3[value]
+    if varname == 'locp': return dic_locp[value]
