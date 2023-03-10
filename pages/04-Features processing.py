afba0b04e268a3ffb2c5a9a0e011b589b3efd3b7
nicolascarayon <nicolas.carayon@gmail.com>
2023-03-10 18:14:14 +0100

    wip

--- pages/04-Features processing.py
+++ pages/04-Features processing.py
@@ -1,3 +1,17 @@
+import streamlit as st
 from pages.libs import comp
 
 comp.sidebar_info()
+
+comp.header("Features processing")
+
+tab1, tab2, tab3 = st.tabs(["Données manquantes", "Relations gravité-variables", "Distributions  conditionnelles"])
+
+with tab1:
+    comp.header("tab1")
+
+with tab2:
+    comp.header("tab2")
+
+with tab3:
+    comp.header("tab3")
