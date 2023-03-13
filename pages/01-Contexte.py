b1d2e47639a65fb15f16dc6c372fd944a9fc3b7e
Nicolas Carayon <nicolas.carayon@gmail.com>
2023-03-13 10:55:13 +0100

    wip

--- pages/01-Contexte.py
+++ pages/01-Contexte.py
@@ -12,19 +12,19 @@ def_gravite =   """
                 ##### Gravité des blessures subies par un usager de la route impliqué dans un accident
                 """
 source_data =   """
-                ##### Fichier BAAC 
-                ##### *Base de données Annuelles des Accidents Corporels de la circulation routière (2005 à 2021)*
+                ##### Fichier BAAC : *Base de données Annuelles des Accidents Corporels de la circulation routière (2005 à 2021)*
                 """
-
 
 comp.header("Contexte")
 st.markdown("---")
 comp.subheader("Objectifs")
 st.markdown(objective)
 
+comp.subheader("Source de données")
+st.markdown(source_data)
+
 comp.subheader("Variable cible")
 st.markdown(def_gravite)
 
-comp.subheader("Source de données")
-st.markdown(source_data)
+
 
