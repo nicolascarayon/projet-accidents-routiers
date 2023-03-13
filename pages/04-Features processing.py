6dcc17c3e472fa1db8210179b1836595d25446a8
nicolascarayon <nicolas.carayon@gmail.com>
2023-03-13 18:36:01 +0100

    Update presentation pages

--- pages/04-Features processing.py
+++ pages/04-Features processing.py
@@ -8,7 +8,23 @@ comp.header("Features processing")
 tab1, tab2, tab3 = st.tabs(["Données manquantes", "Relations gravité-variables", "Distributions  conditionnelles"])
 
 with tab1:
-    comp.header("tab1")
+    comp.header("Traitements")
+    st.markdown("- ##### Suppression des variables considérées inutiles vis à vis du problème (adresse postale, coord; GPS, etc...)")
+    st.markdown("- ##### Suppression des variables avec trop de valeurs *Null*")
+    st.markdown("- ##### Ré-encodage de variables avec trop de modalités (ex : catégorie de véhicules passée de 40 à 7)")
+    st.markdown("- ##### Target encoding de la variable *Département* (en raison du grand nombre de modalités)")
+    st.markdown("- ##### One Hot Encoding de toutes les variables catégorielles")
+    st.markdown("- ##### Ré-encodage de la cible en 2 modalités : 0 - Indemne/Blessé léger vs. 1 - Blessé hospitalisé/Tué")
+    st.markdown("- ##### Ré-équilibrage des classes par algorithme SMOTE (Synthetic Minority Oversampling TEchnique)")
+
+    col1, col2 = st.columns([1, 9])
+    with col1:
+        smote_principle   = st.checkbox("Principe")
+    with col2:
+        smote_application = st.checkbox("Application")
+
+    if smote_principle   : st.image("./pics/smote-principle.png", width=500)
+    if smote_application :  st.image("./pics/smote-application.png", width=500)
 
 with tab2:
     comp.header("tab2")
--- pages/05-Cycle de modélisation.py
+++ pages/05-Cycle de modélisation.py
@@ -38,11 +38,48 @@ with tab_dt:
     st.markdown("- ##### 1. Sélection de la variable qui divise le mieux l'ensemble par rapport à la variable cible (selon un critère défini)")
     st.markdown("- ##### 2. Détermination de la valeur de la variable qui divise l'ensemble en 2 sous-ensembles")
     st.markdown("- ##### 3. Création d'un noeud à partir de la variable et la valeur seuil - Répétition des étapes 1 à 3 sur les sous-ensembles créés")
+
+    show_res_dt = st.checkbox("Résultats", key="chk_res_dt")
+    if show_res_dt:
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.image("./pics/dt-classif-report.png", width=500)
+        with col2:
+            st.image("./pics/dt-confusion.png", width=250)
+
+
 with tab_rf:
     comp.subheader("Random Forest")
+    st.markdown("- ##### Le vote d'un nombre important de classifieurs simples est meilleur que l'appel à un *expert* (aux sens de la précision et de la stabilité)")
+    st.markdown("- ##### Construction d'un grand nombre d'arbres opérant chacun sur un sous ensemble aléatoire du jeu d'entraînement")
+    st.markdown("- ##### La prédiction est le fruit de l'agrégation des prédictions de tous les arbres")
+
+    show_res_rf = st.checkbox("Résultats", key="chk_res_rf")
+    if show_res_rf:
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.image("./pics/rf-classif-report.png", width=500)
+        with col2:
+            st.image("./pics/rf-confusion.png", width=250)
 
 with tab_gb:
     comp.subheader("Gradient Boosting")
 
+    show_res_gb = st.checkbox("Résultats", key="chk_res_gb")
+    if show_res_gb:
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.image("./pics/gb-classif-report.png", width=500)
+        with col2:
+            st.image("./pics/gb-confusion.png", width=250)
+
 with tab_cb:
     comp.header("CatBoost")
+
+    show_res_cb = st.checkbox("Résultats", key="chk_res_cb")
+    if show_res_cb:
+        col1, col2 = st.columns([1, 1])
+        with col1:
+            st.image("./pics/cb-classif-report.png", width=500)
+        with col2:
+            st.image("./pics/cb-confusion.png", width=250)
