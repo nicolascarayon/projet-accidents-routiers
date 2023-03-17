4341de0ec8246840264a31de2072fe80ff005841
Nicolas Carayon <nicolas.carayon@gmail.com>
2023-03-17 13:31:07 +0100

    Rename streamlit page

--- pages/06-Explicabilité - Shapash.py
+++ pages/06-Explicabilité - Shapash.py
@@ -27,8 +27,6 @@ with tab_shap:
         st.image("./pics/logo-maif.png", width=150)
 
 with tab_app_cb:
-
-
     col_btn, col_cb_acc_type, col_empty = st.columns([1,1, 6])
     with col_btn:
         acc_grav_only = st.checkbox(label="Accidents graves")
@@ -36,7 +34,7 @@ with tab_app_cb:
         data_test = pd.read_pickle("./pickles/df-prd-test.pkl")
         X_test = data_test.drop(columns=['grav'], axis=1)
         y_test = data_test.grav
-        X_acc, y_acc, ind = utils.get_random_accident(X_test, y_test, acc_grav_only)
+        X_acc, y_acc, ind, index = utils.get_random_accident(X_test, y_test, acc_grav_only)
 
         st.session_state.sb_dep    = refs.dic_dep[int(X_acc.dep)]
         st.session_state.sb_catv   = refs.dic_catv[int(X_acc.catv)]
@@ -48,54 +46,50 @@ with tab_app_cb:
         st.session_state.sb_catu   = refs.dic_catu[int(X_acc.catu)]
         st.session_state.sb_situ   = refs.dic_situ[int(X_acc.situ)]
         st.session_state.sb_place  = refs.dic_place[int(X_acc.place)]
+
     with col_cb_acc_type:
         if st.button('New accident'):
             st.experimental_rerun()
 
-    col1, col2 = st.columns([1, 4])
+    col1, col2 = st.columns([1, 2])
 
     with col1:
-        dep_val = st.selectbox(label="Département", options=list(refs.dic_dep.values()), key='sb_dep')
-        dep_key = refs.get_key_from_value(refs.dic_dep, dep_val)
+        col_label, col_value = st.columns([0.6, 1])
 
-        catv_val = st.selectbox(label="Catégorie de véhicule", options=list(refs.dic_catv.values()), key='sb_catv')
-        catv_key = refs.get_key_from_value(refs.dic_catv, catv_val)
+        with col_label:
+            st.write("")
+            st.write(f"**Département**")
+            st.write(f"**Catégorie de véhicule**")
+            st.write("**Catégorie de route**")
+            st.write("**Type de collision**")
+            st.write("**Localisation**")
+            st.write("**Motif du déplacement**")
+            st.write("**Mois**")
+            st.write("**Catégorie d'usager**")
+            st.write("**Situation de l'accident**")
+            st.write("**Place de l'usager**")
 
-        catr_val = st.selectbox(label="Catégorie de route", options=list(refs.dic_catr.values()), key='sb_catr')
-        catr_key = refs.get_key_from_value(refs.dic_catr, catr_val)
+        with col_value:
+            st.write("")
+            st.write(f"{refs.dic_dep[int(X_acc.dep)]}")
+            st.write(f"{refs.dic_catv[int(X_acc.catv)]}")
+            st.write(f"{refs.dic_catr[int(X_acc.catr)]}")
+            st.write(f"{refs.dic_col[int(X_acc.col)]}")
+            st.write(f"{refs.dic_agg[int(X_acc['agg'])]}")
+            st.write(f"{refs.dic_trajet[int(X_acc.trajet)]}")
+            st.write(f"{refs.dic_mois[int(X_acc.mois)]}")
+            st.write(f"{refs.dic_catu[int(X_acc.catu)]}")
+            st.write(f"{refs.dic_situ[int(X_acc.situ)]}")
+            st.write(f"{refs.dic_place[int(X_acc.place)]}")
 
-        col_val = st.selectbox(label="Type de collision", options=list(refs.dic_col.values()), key='sb_col')
-        col_key = refs.get_key_from_value(refs.dic_col, col_val)
-
-        agg_val = st.selectbox(label="Localisation", options=list(refs.dic_agg.values()), key='sb_agg')
-        agg_key = refs.get_key_from_value(refs.dic_agg, agg_val)
-
-        trajet_val = st.selectbox(label="Motif du déplacement", options=list(refs.dic_trajet.values()), key='sb_trajet')
-        trajet_key = refs.get_key_from_value(refs.dic_trajet, trajet_val)
-
-        mois_val = st.selectbox(label="Mois", options=list(refs.dic_mois.values()), key='sb_mois')
-        mois_key = refs.get_key_from_value(refs.dic_mois, mois_val)
-
-        catu_val = st.selectbox(label="Catégorie d'usager", options=list(refs.dic_catu.values()), key='sb_catu')
-        catu_key = refs.get_key_from_value(refs.dic_catu, catu_val)
-
-        situ_val = st.selectbox(label="Situation de l'accident", options=list(refs.dic_situ.values()), key='sb_situ')
-        situ_key = refs.get_key_from_value(refs.dic_situ, situ_val)
-
-        place_val = st.selectbox(label="Place de l'usager", options=list(refs.dic_place.values()), key='sb_place')
-        place_key = refs.get_key_from_value(refs.dic_place, place_val)
-
-    # X = [dep_key, catv_key, catr_key, col_key, agg_key, trajet_key, mois_key, catu_key, situ_key, place_key]
 
     with col2:
-        tab_results, tab_empty = st.tabs(["Prédiction", "..."])
+        tab_prediction, tab_empty = st.tabs(["Prédiction", "..."])
 
-        with tab_results:
+        with tab_prediction:
             col_graph_densities, col_diagnosis = st.columns([1,1])
             with col_graph_densities:
                 fig, y_pred_proba, y_true = utils.plot_prob_densities(model, X_test, y_test, ind)
-                st.pyplot(fig)
-            with col_diagnosis:
                 pred = "Grave" if y_pred_proba >= 0.5 else "Non grave"
                 vraie_valeur = "Grave" if y_true==1 else "Non grave"
 
@@ -105,19 +99,30 @@ with tab_app_cb:
                 else:
                     st.error(f"Prédiction : {pred} - Vraie valeur : {vraie_valeur}")
 
+
+                st.pyplot(fig)
+            with col_diagnosis:
+
+
                 if 'xpl' not in st.session_state:
-                    print('load smart explainer')
                     st.session_state['xpl'] = utils.get_smart_xpl(model, X_test, y_test)
+                    y_pred = pd.Series(model.predict(X_test))
+                    y_pred.index = X_test.index
+                    st.session_state['xpl'].compile(
+                        x=X_test,
+                        y_pred=y_pred, # Optional: for your own prediction (by default: model.predict)
+                        y_target=y_test, # Optional: allows to display True Values vs Predicted Values
+                    )
+                    if 'summary_df' not in st.session_state:
+                        st.session_state['summary_df'] = st.session_state['xpl'].to_pandas(proba=False, max_contrib=10)
 
                 if st.session_state['xpl'] is not None:
-                    fig_loc_expl = utils.get_local_explanation_fig(st.session_state.xpl, ind)
-                    st.title("Shapash Local Explanation")
-                    st.write("Individual index:", ind)
-                    st.pyplot(fig_loc_expl)
-                    # print("should plot here...")
-                    # print(type(st.session_state['fig_loc_expl']))
-                    # st.pyplot(st.session_state['fig_loc_expl'])
-                    # st.session_state['xpl'].plot.local_plot(row_num=0)
+                    xpl = st.session_state['xpl']
+                    df_loc_summary = st.session_state.summary_df
+                    st.subheader("Shapash Local Explanation")
+                    # st.dataframe(data=df_loc_summary.loc[index])
+                    fig = utils.get_local_summary_plot(df_loc_summary.loc[index])
+                    st.pyplot(fig)
 
 
     with tab_empty:
